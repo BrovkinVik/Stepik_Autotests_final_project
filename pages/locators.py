90f5a547c96e20b7d24a0838e478db1a36093233
BrovkinVik <vikbrovkin@gmail.com>
2020-09-20 23:09:47 +0500

    Added files: test_product_page.py pages/product_page.py; Changed files:pages/base_page.py pages/locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
 	LOGIN_SUBMIT = (By.CSS_SELECTOR, "[name='login_submit']")
 	REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "[name='registration_submit']")
+
+class ProductPageLocators():
+	BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+	MESSAGE_ADDED_TO_BASKET = (By.CSS_SELECTOR, "#messages > div.alert-success:nth-child(1) .alertinner strong")
+	BASKET_PRICE = (By.CSS_SELECTOR, "#messages > div.alert-info p strong")
+	PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main .price_color")
+	PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
